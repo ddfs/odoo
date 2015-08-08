f4ee4dbb6050efd696d092cd672b61b23a06c7aa
DD FS <fatihpiristine@gmail.com>
2015-08-08 23:57:08 +0300

    Update hxn_main.py

--- addons/website/controllers/hxn_main.py
+++ addons/website/controllers/hxn_main.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
-
-import logging
-
 from openerp.addons.web import http
 from openerp.http import request
 from openerp.addons.website.controllers.main import Website
 
-
-logger = logging.getLogger(__name__)
-
-
-class HxnWebsite(Website):
+class Website(Website):
     @http.route("/website/info", type="http", auth="public", website=True)
     def website_info(self):
         # show 404
-        logger.info("website.info is disabled")
+        # logger.info("website.info is disabled")
         return request.website.render("website.404")
