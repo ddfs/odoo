b9dbc885331bb2a423f8098771bab92d67059b62
DD FS <fatihpiristine@gmail.com>
2017-08-21 19:36:28 +0300

    Update and rename addons/sitemap/sitemap.py to modules-08/website_sitemap/models/website.py

--- modules-08/website_sitemap/models/website.py
+++ modules-08/website_sitemap/models/website.py
@@ -1,6 +1,6 @@
 from openerp.osv import fields, osv
 
-class website(osv.osv):
+class Website(osv.osv):
     _inherit = 'website'
 
     _columns = {
@@ -8,13 +8,11 @@ class website(osv.osv):
     }
 
 
-class website_config_settings(osv.osv_memory):
+class WebsiteConfigSettings(osv.osv_memory):
     _inherit = 'website.config.settings'
 
     _columns = {
         'exclude_from_sitemap': fields.related('website_id', 'exclude_from_sitemap', type="text",
                                                string='Exclude from sitemap',
                                                help="Enter the urls to be excluded from sitemap")
-
     }
-
